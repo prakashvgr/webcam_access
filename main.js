7497bc3234faff89b1f93be1433cad8a00fd9252
Prakash Rajendran <prakash.rajendran@capgemini.com>
2017-03-22 15:07:46 +0530

    server port updated

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var path = require('path');
 
+var port = process.env.PORT || 1337;
+
 var app = express();
 
 app.use(express.static(path.join(__dirname, '/')));
 
-app.listen(8080, function() {
-	console.log('Application is running on port 3000');
+app.listen(port, function() {
+	console.log('Application is running on port %s', port);
 });
